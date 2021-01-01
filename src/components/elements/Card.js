7c5d20f6264df4fb9e524084fd2ff78282d9f074
hannasukharieva <annsukhareva33@gmail.com>
2021-01-01 15:35:50 +0000

    Minor bug fix.

--- src/components/elements/Card.js
+++ src/components/elements/Card.js
@@ -4,7 +4,7 @@ function Card(props) {
   return (
     <a href={props.link} target="_blank" rel="noreferrer">
       <div className="card">
-        <img className="card__photo" alt="photo-image" src={props.image}/>
+        <img className="card__photo" alt="A Polaroid card with visuals." src={props.image}/>
         <p className="card__text">{props.sign}</p>
       </div>
     </a>
