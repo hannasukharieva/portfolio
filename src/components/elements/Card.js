ca47b2ad5f241bd13498c6e41c4915b17402f6fc
hannasukharieva <annsukhareva33@gmail.com>
2021-01-01 15:30:56 +0000

    Added alternative captions to images

--- src/components/elements/Card.js
+++ src/components/elements/Card.js
@@ -4,7 +4,7 @@ function Card(props) {
   return (
     <a href={props.link} target="_blank" rel="noreferrer">
       <div className="card">
-        <img className="card__photo" src={props.image}/>
+        <img className="card__photo" alt="photo-image" src={props.image}/>
         <p className="card__text">{props.sign}</p>
       </div>
     </a>
